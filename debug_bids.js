86612453edf22d84d63a9f58c92d483f798843d5
bmore <bmore@BMORE-MACBOOK.local>
2018-04-04 13:58:03 -0400

    bid debuggging

---
+++ debug_bids.js
@@ -0,0 +1,33 @@
+var ix_bid = {
+        ad: '<iframe src="http://a3843.casalemedia.com/ifnotify?c=F21739&r=6E25D0C5&t=5AC4EEBF&u=V0d3bkdNQW9Kbm9BQUFaSUZBWUFBQUU1&m=377b7f5a9537714758c6990a34007ca6&wp=4A&aid=BFD4DFC781B0ECE9&tid=11F3F&s=2ADB3&cp=0.74&n=thestir.brandonm.dev.cafemom.com&pr=xx&sid=1a14-1b37-1c37-1c38-1c3b-1c3c-1c5c-1c5d-1c5e-1c62-1c77-1c81-1cd9-1cda-1cdb-1df1-1e66-2155-2156-215e-2163-218e-2193-21be-21bf-223b-2250-2251-2252-2295-2296-22ca-24bc-270e-27ef-2aaa-2bd6-2c80-2cf4-2e59-2f46-2f52-2f5d-2fc6-326b-32ca-344e-344f-38bd-38bf-3a18-3a19-3a53-3d57-4113-4120-4127-438f-43db-441d-45b8-45bb-45bd-45c0-4981-49a7-49ae-49ce-49dc-4cc7-4d6e-4d78-4da8-4dab-4db0-4fd6-51d0-5c09-5cc3-5cc4-5cc7-5cd3-602b-602c&epr=303063140" width="0" height="0" frameborder="0" scrolling="no" style="display:none;" marginheight="0" marginwidth="0"></iframe><scr'+'ipt type="text/javascript" src="http://pr.ybp.yahoo.com/ab/secure/false/imp/VONOZAxJo2ETHtD9Hg27Hg2OtlHRDYq3sTe8p1gG1g9sEQ2ewo_tQtmnqfrQsDcp7z17WYHRn5_FELXkC-FDDhF04yelXQg-mWz8DH6L3nJCdLQen5yGi3GyxEMVN97i1v1Y8qUz0MLz3mfkQXoh24T62mSd0aV5QLxrtsy-H6cQXGhkFwQLnw2nEv8RyLliZ3xnFfmj38jqFh_txB6wR4YZZJnDNQMq9tNqiH1BsP2ap2k1Qfi1Tj1rYdsCT5oOFLYT4jLxTVZLONf-DzW3wYQT6FKhK2iRbQm1D-rsIRfaEyadFOFGfEk9DKxm8NxCQj6tuYBTIY0-DROoLPX79Vk9XfaoKPtVKVduuWj8JKsBZvop5k7hUL-xdM79LLjoIIM1-GUrZUOaD3vxopVpf5DQNYhqGEV6LDWlcOGF0MK2G-YRmZRuZvBE0vIhDLUzeUQdJlX-_DjCRQJ7s6UZoFkNcd-5HLsFqU6uS42Lby0lWM5k8y8qhk0z3_8p8s-BLxuke7QSJ8r1_X2prZX9rmwWTHHlvfvNMEwsZh4QC6p9Rdv6F-e_-s1Wu7opB_t7W5ZGhtWFVpD1pblrGhIuPXBNHR_ZxcttFKf1evs8DJ7D9Ula1tuWxC70w8SC0hbMTj_tDTsoDzB448WUAX9s7jS66AYB3cHmFLMTK9HbgQrHx3beG8fTporL-fYdh5BFlu2LRao8ft9FXs8f3b5S6ntjprymY6s4N0QtNyvAwNEicxnehZZE4_hE-jvep9bfonZUy7q3v0C-9UAtF1vpOMXBqcBaqbduB8pGyTxv5HJo3s-EafRx_ukvKSJOr34V-3-onTgwVV0kLdxjVNZCCujdtPqhm9TTy0v-QQDCrpmdwBT8sQIs0GEQIdP0meU4sBMryX7ldPnnGDRac9k3wbNt8C_GyTws4xkd8sd7m84JRQdU6DTvXm5h6oLwQZipETgNNQINReNZM_A8T3MH5CqUV6Eh3oOxT8hCf9yfR44yGkEpbdCv0Vq7OIUllb-2lf1l1QQWJrHkxL0WSIWSd3IYluG2xgDWWZKVNn1RXvFq3pJ6KBGtVYjKbojh6eBlLF5vqFTClXgBvC6XZ2JQrOxPSx_NelV1BLj-18CYQe6NGOyaDt4N10w-4vy-4fvLu904Epvpe5YSTnjzz7fsf0Y8wHGzkZwutN69nSYC1jqpukUJSHxlZcHhzs32EHZQfcnS5N4Gk5iw5Bvu01KytsbNxS6Z91iNo-lS7vjiRCwIKPHPi7Xi_VvmfsrW6mN_kM97Yvx0ZNQOHqIAsXOLgNor8TPloOkq/wp/0.74"></scr'+'ipt>',
+        adId : "977a2e7ce570af1",
+        adUnitCode : "div-gpt-ad-840786271-1",
+        adserverTargeting : {O: "1_60", P: "", hb_adid: "977a2e7ce570af1", hb_bidder: "indexExchange", hb_pb: "0.60", hb_size: "728x90"},
+        alwaysUseBid : true,
+        bidder : "indexExchange",
+        bidderCode : "indexExchange",
+        cpm : 0.60,
+        //cached: true,
+        getSize : function(){return this.width+"x"+this.height},
+        getStatusCode : function(){return i},
+        height : 90,
+        mediaType : "banner",
+        pbAg : "0.60",
+        pbCg : "",
+        pbDg : "0.60",
+        pbHg : "0.60",
+        pbLg : "0.50",
+        pbMg : "0.60",
+        requestId : "051fef13-ef7f-4d8a-bf5a-380f9c29b2f6",
+        requestTimestamp : new Date().getTime(),//1522855615832,
+        responseTimestamp : new Date().getTime() + 500,//1522855616042,
+        sendStandardTargeting : undefined,
+        siteID : 175539,
+        size : "728x90",
+        statusMessage : "Bid available",
+        timeToRespond : 210,
+        width : 728
+    }
+    pbjs._bidsReceived = [];
+    //pbjs.addBidResponse(ix_bid.adUnitCode,ix_bid);
+    pbjs._bidsReceived.push(ix_bid);
